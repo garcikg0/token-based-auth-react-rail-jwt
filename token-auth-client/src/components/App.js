8e14c2cf38209b0415be28226705d31b1748992c
garcikg0 <kg.gar91@gmail.com>
2020-10-21 23:01:32 -0400

    controllers refactor

---
+++ token-auth-client/src/components/App.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom'
+import SignUp from './SignUp'
+import Login from './Login'
+import NavBar from './NavBar'
+import Profile from './Profile'
+
+class App extends React.Component {
+  state = {
+    currentUser: null
+  }
+
+  // log user in when component mounts
+  componentDidMont() {
+    // check if user is logged in 
+    if (localStorage.token) {
+      fetch(`http://localhost:3000/autologin`, {
+        headers: {
+          "Authorization": `Bearer ${localStorage.token}`
+        }
+      })
+        .then(r => r.json())
+        .then(data => {
+          // check for errors (could also check the status code of the response)
+          if (!data.error)
+              // and set current user in state
+          this.handleLogin(data)
+        })
+    }
+  }
+  
+  handleLogin = currentUser => {
+    //set current user, then redirect to home page
+    this.setState({ currentUser }, () => {
+      this.props.history.push('/home')
+    })
+  }
+
+  handleLogout = () => {
+    // TODO: remove the userId from localStorage
+    localStorage.removeItem("token")
+    // and clear the user in state 
+    this.setState({
+      currentUser: null
+    })
+  }
+
+  render() {
+    console.log("In App, state:", this.state)
+    return (
+      <>
+        <NavBar currentUser={this.state.currentUser} handleLogout={this.handleLogout} />
+        <main>
+          <Switch>
+            <Route path="/signup">
+              <SignUp handleLogin={this.handleLogin} />
+            </Route>
+            <Route path="/login">
+              <Login handleLogin={this.handleLogin} />
+            </Route>
+            <Route path="/profile">
+              {this.state.currentUser ? <Profile currentUser={this.state.currentUser} updateUser={this.updateUser} /> : <Redirect to='/' />}
+            </Route>
+            <Route path="/home">
+              {this.state.currentUser ? <h1>Welcome, {this.state.currentUser.username}</h1> : <Redirect to='/' />}
+            </Route>
+            <Route path="/">
+              <h1>Please Login or Sign Up</h1>
+            </Route>
+          </Switch>
+        </main>
+      </>
+    );
+  }
+}
+
+export default withRouter(App);
